25b5b3500edea4080d615961a11bd9c3eb2d549c
naolArega <na408498@gmail.com>
2022-03-15 14:50:46 +0300

    included api routes to the app

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from routers import pet_router, customer_router, adoption_router, report_router
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"message": "Welcome to buchi api"}
+
+app.include_router(pet_router.router)
+app.include_router(customer_router.router)
+app.include_router(adoption_router.router)
+app.include_router(report_router.router)
