f95914496ee729c9f383405d0ad53bdd00bfc3c2
naolArega <na408498@gmail.com>
2022-03-17 21:05:40 +0300

    fixed cors issue

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from routers import pet_router, customer_router, adoption_router, report_router
 
 app = FastAPI(title="Buchi", version="1.0.0 beta", description="Buchi, a pet discovery and adoption platform")
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 @app.get("/")
 def index():
